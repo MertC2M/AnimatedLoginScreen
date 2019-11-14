b37de51b2416d1e419b4614c0012de60f90a9763
Mert <mert@connected2.me>
2019-11-13 18:46:54 -0800

    Full covered Image and Buttons added.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import {Image, StatusBar, StyleSheet, View, Text} from 'react-native';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 export default class LoginScreen extends React.Component {
   constructor() {
     super();
@@ -7,8 +16,24 @@ export default class LoginScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>test</Text>
+      <View
+        style={{flex: 1, backgroundColor: 'white', justifyContent: 'flex-end'}}>
+        <View style={{...StyleSheet.absoluteFill}}>
+          <Image
+            source={require('../assets/bg.jpg')}
+            style={{flex: 1, height: null, width: null}}
+          />
+        </View>
+        <View style={{height: height / 3}}>
+          <View style={styles.button}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>SIGN IN</Text>
+          </View>
+          <View style={{...styles.button, backgroundColor: '#2E71DC'}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+              SIGN IN WITH Connected2.me
+            </Text>
+          </View>
+        </View>
       </View>
     );
   }
@@ -20,5 +45,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: 'white',
+    height: 70,
+    marginHorizontal: 20,
+    borderRadius: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
   },
 });
